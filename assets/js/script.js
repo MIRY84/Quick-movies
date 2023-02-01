26f79cf2270fc5f382771770771ff7d4c6ea4ea0
mihacuo <mihhail.odnolko@gmail.com>
2023-02-01 20:30:16 +0000

    added title and removed key from the main script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ console.log("script.js linked");
 
 // function gets a seacrhTerm and searched for movies
 function getMoviesInfo(searchTerm) {
-  var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&apikey=trilogy";
+  var queryURL = "https://www.omdbapi.com/?t=" + searchTerm + "&apikey=" + omdbApikey;
   $.ajax({
     url:queryURL,
     method: "GET"
@@ -12,14 +12,16 @@ function getMoviesInfo(searchTerm) {
     var poster = response.Poster;
     var year = response.Year;
     var rating = response.imdbRating;
+    var title = response.Title;
     console.log(poster, year, rating);
     // create image, and 2 x paragraphs
     var img = $("<img>");
     img.attr("src", poster);
     var yearInfo = $("<p>").text(year);
     var ratingInfo = $("<p>").text(rating);
+    var titleInfo = $("<p>").text(title);
     // append all elements to the card
-    $("body").append(img, yearInfo, ratingInfo);
+    $("body").append(titleInfo, img, yearInfo, ratingInfo);
 
 
   })
